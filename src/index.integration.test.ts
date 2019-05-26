1c04dd920ad87f06c014a77de154c6912c0e3a2a
en-ken <inonakano.kawaz.taikai.never.knows@gmail.com>
2019-05-26 18:53:27 +0900

    test: add a app test.

--- src/index.integration.test.ts
+++ src/index.integration.test.ts
@@ -12,7 +12,7 @@ function cli(args: any[] = []) {
   });
 }
 
-describe('CLI displays', () => {
+describe('App test:', () => {
   const saveDir = '.tmp';
   beforeEach(() => {
     try {
@@ -24,47 +24,63 @@ describe('CLI displays', () => {
   afterEach(() => {
     rimraf(saveDir, () => {});
   });
-  test('help', () => {
-    const result = cli(['-h']);
+  test('outputs the expected file', () => {
+    const inputPath = 'test-data/petstore.template.yml';
+    const outputPath = `${saveDir}/result.yml`;
+    const expectedPath = 'test-data/petstore.expanded.yml';
+    cli([inputPath, outputPath]);
 
-    const match = [
-      /Usage: json-yaml-ref-resolver \[options\] <targetFilePath> <outputFilePath>/,
-      /-i, --indent <size>/,
-      /-h, --help/
-    ];
-    match.forEach(x => expect(result.stdout).toMatch(x));
+    while (!fs.existsSync(outputPath)) {
+      setTimeout(() => {}, 100);
+    }
+
+    const expected = fs.readFileSync(expectedPath, 'utf-8');
+    const actual = fs.readFileSync(outputPath, 'utf-8');
+    expect(actual).toEqual(expected);
   });
-  test('usage when required input is not enough', () => {
-    const result = cli();
-    const match = [
-      /Usage: json-yaml-ref-resolver \[options\] <targetFilePath> <outputFilePath>/,
-      /-i, --indent <size>/,
-      /-h, --help/
-    ];
-    match.forEach(x => expect(result.stdout).toMatch(x));
-  });
-  test('the success message', () => {
-    const result = cli([
-      'test-data/petstore.template.yml',
-      `${saveDir}/result.yml`
-    ]);
-    expect(result.stderr).toEqual('');
-    expect(result.stdout).toMatch(/Done./);
-  });
-  test('the error message when <targetFilePath> does not exist', () => {
-    const result = cli(['foo.yml', 'bar.yml']);
-    expect(result.stderr).toMatch(/<targetFilePath> does not exist/);
-  });
-  test('the error message when the directory of <outputFilePath> does not exist', () => {
-    const result = cli(['test-data/petstore.template.yml', 'foo/bar.yml']);
-    expect(result.stderr).toMatch(/<outputFilePath> does not exist/);
-  });
-  test('the error message when the extension of <targetFilePath> is wrong', () => {
-    const result = cli(['foo.xml', 'bar.yml']);
-    expect(result.stderr).toMatch(/Extension must be json\/yaml\/yml/);
-  });
-  test('the error message when the extension of <outputFilePath> is wrong', () => {
-    const result = cli(['test-data/petstore.template.yml', 'bar.xml']);
-    expect(result.stderr).toMatch(/Extension must be json\/yaml\/yml/);
+  describe('CLI displays', () => {
+    test('help', () => {
+      const result = cli(['-h']);
+
+      const match = [
+        /Usage: json-yaml-ref-resolver \[options\] <targetFilePath> <outputFilePath>/,
+        /-i, --indent <size>/,
+        /-h, --help/
+      ];
+      match.forEach(x => expect(result.stdout).toMatch(x));
+    });
+    test('usage when required input is not enough', () => {
+      const result = cli();
+      const match = [
+        /Usage: json-yaml-ref-resolver \[options\] <targetFilePath> <outputFilePath>/,
+        /-i, --indent <size>/,
+        /-h, --help/
+      ];
+      match.forEach(x => expect(result.stdout).toMatch(x));
+    });
+    test('the success message', () => {
+      const result = cli([
+        'test-data/petstore.template.yml',
+        `${saveDir}/result.yml`
+      ]);
+      expect(result.stderr).toEqual('');
+      expect(result.stdout).toMatch(/Done./);
+    });
+    test('the error message when <targetFilePath> does not exist', () => {
+      const result = cli(['foo.yml', 'bar.yml']);
+      expect(result.stderr).toMatch(/<targetFilePath> does not exist/);
+    });
+    test('the error message when the directory of <outputFilePath> does not exist', () => {
+      const result = cli(['test-data/petstore.template.yml', 'foo/bar.yml']);
+      expect(result.stderr).toMatch(/<outputFilePath> does not exist/);
+    });
+    test('the error message when the extension of <targetFilePath> is wrong', () => {
+      const result = cli(['foo.xml', 'bar.yml']);
+      expect(result.stderr).toMatch(/Extension must be json\/yaml\/yml/);
+    });
+    test('the error message when the extension of <outputFilePath> is wrong', () => {
+      const result = cli(['test-data/petstore.template.yml', 'bar.xml']);
+      expect(result.stderr).toMatch(/Extension must be json\/yaml\/yml/);
+    });
   });
 });
