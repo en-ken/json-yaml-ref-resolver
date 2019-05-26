7f25082cde0d98cdbf292a6b1416615b8e51b780
en-ken <inonakano.kawaz.taikai.never.knows@gmail.com>
2019-05-26 14:54:52 +0900

    tests: change beforeAll to beforeEach.

--- src/index.integration.test.ts
+++ src/index.integration.test.ts
@@ -1,38 +1,31 @@
-import { exec } from 'child_process';
+import { spawnSync } from 'child_process';
 import path from 'path';
 import fs from 'fs';
 import rimraf from 'rimraf';
 
-async function cli(args: any[] = []) {
-  return new Promise<{ code: number; stdout: string; stderr: string }>(
-    resolve =>
-      exec(
-        `node ${path.resolve('./')} ${args.join(' ')}`,
-        { cwd: '.' },
-        (err, stdout, stderr) =>
-          resolve({
-            code: err && err.code ? err.code : 0,
-            stdout,
-            stderr
-          })
-      )
-  );
+function cli(args: any[] = []) {
+  return spawnSync('npx', ['ts-node', path.resolve('./'), ...args], {
+    cwd: process.cwd(),
+    env: process.env,
+    stdio: 'pipe',
+    encoding: 'utf-8'
+  });
 }
 
 describe('CLI displays', () => {
   const saveDir = '.tmp';
-  beforeAll(() => {
+  beforeEach(() => {
     try {
       fs.statSync(saveDir);
     } catch (_) {
       fs.mkdirSync(saveDir);
     }
   });
-  afterAll(() => {
+  afterEach(() => {
     rimraf(saveDir, () => {});
   });
-  test('help', async () => {
-    const result = await cli(['-h']);
+  test('help', () => {
+    const result = cli(['-h']);
 
     const match = [
       /Usage: json-yaml-ref-resolver \[options\] <targetFilePath> <outputFilePath>/,
@@ -41,8 +34,8 @@ describe('CLI displays', () => {
     ];
     match.forEach(x => expect(result.stdout).toMatch(x));
   });
-  test('usage when required input is not enough', async () => {
-    const result = await cli();
+  test('usage when required input is not enough', () => {
+    const result = cli();
     const match = [
       /Usage: json-yaml-ref-resolver \[options\] <targetFilePath> <outputFilePath>/,
       /-i, --indent <size>/,
@@ -50,46 +43,27 @@ describe('CLI displays', () => {
     ];
     match.forEach(x => expect(result.stdout).toMatch(x));
   });
-  test('the success message', async () => {
-    const result = await cli([
+  test('success message', () => {
+    const result = cli([
       'test-data/petstore.template.yml',
       `${saveDir}/result.yml`
     ]);
     expect(result.stdout).toMatch(/Done./);
   });
-  test('the success message with -i', async () => {
-    const result = await cli([
-      '-i 4',
-      'test-data/petstore.template.yml',
-      `${saveDir}/result-i.yml`
-    ]);
-    expect(result.stdout).toMatch(/Done./);
-  });
-  test('the success message with --indent', async () => {
-    const result = await cli([
-      '--indent 4',
-      'test-data/petstore.template.yml',
-      `${saveDir}/result-indent.yml`
-    ]);
-    expect(result.stdout).toMatch(/Done./);
-  });
-  test('the error message when <targetFilePath> does not exist', async () => {
-    const result = await cli(['foo.yml', 'bar.yml']);
+  test('the error message when <targetFilePath> does not exist', () => {
+    const result = cli(['foo.yml', 'bar.yml']);
     expect(result.stderr).toMatch(/<targetFilePath> does not exist/);
   });
-  test('the error message when the directory of <outputFilePath> does not exist', async () => {
-    const result = await cli([
-      'test-data/petstore.template.yml',
-      'foo/bar.yml'
-    ]);
+  test('the error message when the directory of <outputFilePath> does not exist', () => {
+    const result = cli(['test-data/petstore.template.yml', 'foo/bar.yml']);
     expect(result.stderr).toMatch(/<outputFilePath> does not exist/);
   });
-  test('the error message when the extension of <targetFilePath> is wrong', async () => {
-    const result = await cli(['foo.xml', 'bar.yml']);
+  test('the error message when the extension of <targetFilePath> is wrong', () => {
+    const result = cli(['foo.xml', 'bar.yml']);
     expect(result.stderr).toMatch(/Extension must be json\/yaml\/yml/);
   });
-  test('the error message when the extension of <outputFilePath> is wrong', async () => {
-    const result = await cli(['test-data/petstore.template.yml', 'bar.xml']);
+  test('the error message when the extension of <outputFilePath> is wrong', () => {
+    const result = cli(['test-data/petstore.template.yml', 'bar.xml']);
     expect(result.stderr).toMatch(/Extension must be json\/yaml\/yml/);
   });
 });
